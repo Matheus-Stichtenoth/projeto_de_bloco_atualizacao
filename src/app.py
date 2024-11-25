c1b55a9b7d18a49e6bba5322f94479b3183a7a2d
Matheus_Stick <matheus.stichtenoth@al.infnet.edu.br>
2024-11-25 00:17:04 -0300

    Adicionado sys no app.py para solucao de erro streamlit

--- src/app.py
+++ src/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Adiciona o diretório do projeto ao caminho do Python
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
